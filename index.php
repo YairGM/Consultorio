91a8b0313c2e67193eedcfa019bc2f63f90474c1
YairGM <yagamo@gmail.com>
2017-11-16 20:39:09 -0600

    Editing files

--- index.php
+++ index.php
@@ -1,12 +1,55 @@
-<?php
-header( 'Location: /index.html' ) ;  
-$dsn = "pgsql:"
-    . "host=ec2-107-22-167-179.compute-1.amazonaws.com;"
-    . "dbname=d3c4fd6uvu77r5;"
-    . "user=d3c4fd6uvu77r5;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=a1b95e6ba14e1ad1be10adc991f17237a481349d8498508e75d9b50eaa589632";
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Consultorio medico</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-gv0oNvwnqzF6ULI9TVsSmnULNb3zasNysvWwfT/s4l8k5I+g6oFz9dye0wg3rQ2Q" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylee.css">
 
-$db = new PDO($dsn);
+</head>
+<body>
+    <div class="container">
+        <div class="page-header">
+            <h2>
+                Consultorio medico
+            </h2>
+        </div>
+        <div class="Menu">
+        <ul>
+            <li><a href="#home">Inicio</a></li>
+            <li><a href="#alta">Alta</a></li>
+            <li><a href="#consulta">Consulta</a></li>
+        </ul>
+        </div>
+        <table>
+   <thead>
+    <tr>
+     <th>Employee ID</th>
+     <th>Last Name</th>
+     <th>First Name</th>
+     <th>Title</th>
+    </tr>
+   </thead>
+   <tbody>
+<?php
+$query = "SELECT employee_id, last_name, first_name, title "
+     . "FROM employees ORDER BY last_name ASC, first_name ASC";
+$result = $db->query($query);
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    echo "<tr>";
+    echo "<td>" . $row["employee_id"] . "</td>";
+    echo "<td>" . htmlspecialchars($row["last_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["first_name"]) . "</td>";
+    echo "<td>" . htmlspecialchars($row["title"]) . "</td>";
+    echo "</tr>";
+}
+$result->closeCursor();
 ?>
+   </tbody>
+  </table>
+        <div class="footer">
+            <p>Footer</p>
+        </div>
+    </div>
+</body>
+</html>>
