18384e0b595517612ed29eb6b642b0d07135964f
YairGM <yagamo@gmail.com>
2017-11-16 19:21:13 -0600

    Editing files

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+header( 'Location: /index.html' ) ;  
+$dsn = "pgsql:"
+    . "host=ec2-107-22-167-179.compute-1.amazonaws.com;"
+    . "dbname=d3c4fd6uvu77r5;"
+    . "user=d3c4fd6uvu77r5;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=a1b95e6ba14e1ad1be10adc991f17237a481349d8498508e75d9b50eaa589632";
+
+$db = new PDO($dsn);
+?>
