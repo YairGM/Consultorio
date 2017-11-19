7aa2e79b1f0e660331ba0da5a648ad280fd0c635
YairGM <yagamo@gmail.com>
2017-11-19 14:35:16 -0600

    validarN

--- validar.php
+++ validar.php
@@ -11,6 +11,8 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $result = pg_query($pg_conn, "SELECT Usuario, Contrasena"
      . "FROM usuario");
 
+if (!pg_num_rows($result)) {
+  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 if($row = pg_fetch_row($result)){
 	if($row['2'] ==  $pass){
 		session_start();
@@ -26,6 +28,7 @@ if($row = pg_fetch_row($result)){
 	    echo "</tr>";
 		exit();
 	}
+}
 }else{
 	//header("Location: index.php");
 	exit();
