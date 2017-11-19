f878e9646da1e2686f491fc6710daf08b80a0c88
YairGM <yagamo@gmail.com>
2017-11-19 14:48:44 -0600

    validarN

--- validar.php
+++ validar.php
@@ -2,6 +2,11 @@
 $usuario = $_POST['1'];
 $pass = $_POST['2'];
 
+if(empty($usuario) || empty($pass)){
+	header("Location: index.html");
+	exit();
+}
+
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
