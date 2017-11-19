270233a4e7c064fcf790ad596f4520e01546a152
YairGM <yagamo@gmail.com>
2017-11-19 13:07:52 -0600

    validar

---
+++ validar.php
@@ -0,0 +1,25 @@
+<?php
+
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$result = pg_query($pg_conn, "SELECT Usuario"
+     . "FROM usuario");
+
+if($row = pg_fetch_row($result)){
+	if($row['Contrasena'] ==  $pass){
+		session_start();
+		$_SESSION['Usuario'] = $usuario;
+		header("Location: contenido.php");
+	}else{
+		header("Location: index.php");
+		exit();
+	}
+}else{
+	header("Location: index.php");
+	exit();
+}
+?>
