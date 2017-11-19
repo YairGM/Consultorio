0cc6e52aa81d980fcc1c3c25f68d3a7a2cd2f374
YairGM <yagamo@gmail.com>
2017-11-19 13:04:59 -0600

    login2

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <!DOCTYPE html>
-<?php
-
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-$result = pg_query($pg_conn, "SELECT employee_id, last_name, first_name, title "
-     . "FROM employees ORDER BY last_name ASC, first_name ASC");
-
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  while ($row = pg_fetch_row($result)) {
-      echo "<tr>";
-      echo "<td>" . $row[0] . "</td>";
-      echo "<td>" . htmlspecialchars($row[1]) . "</td>";
-      echo "<td>" . htmlspecialchars($row[2]) . "</td>";
-      echo "<td>" . htmlspecialchars($row[3]) . "</td>";
-      echo "</tr>";
-  }
-}
-?>
 <html>
 <head>
     <meta charset="utf-8">
@@ -32,6 +8,7 @@ if (!pg_num_rows($result)) {
 
 </head>
 <body>
+
     <div class="container">
         <div class="page-header">
             <h2>
@@ -59,33 +36,16 @@ if (!pg_num_rows($result)) {
   </table>
   <h2>Modal Login Form</h2>
 
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+<center>
+		<form method="POST" action="validar.php">
+			<input type="text" name="nnombre" placeholder="Usuario" />
+			<br />
+			<input type="password" name="npassword" placeholder="Contraseña" />
+			<br />
+			<button type="submit">Inicar Sesion</button>
+		</form>
+	</center>
 
-<div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="/action_page.php">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        
-      <button type="submit">Login</button>
-      
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      
-    </div>
-  </form>
-</div>
         <div class="footer">
             <p>Footer</p>
         </div>
