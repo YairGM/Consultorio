2d1379f0f5cb8dbe200e24be281e79e4cbb86b63
YairGM <yagamo@gmail.com>
2017-11-19 15:12:10 -0600

    validarN

--- validar.php
+++ validar.php
@@ -11,7 +11,6 @@ $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $result = pg_query($pg_conn, "SELECT Usuario"
      . "FROM usuario");
 
-header("Location: contenido.php");
 
 if($row = pg_fetch_row($result)){
 	if($row['Contrasena'] ==  $pass){
