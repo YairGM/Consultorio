bcf1fd7195c4000069da6a55f5df470da0d0f5af
YairGM <yagamo@gmail.com>
2017-11-20 16:45:42 -0600

    Editing files

--- consultaE.php
+++ consultaE.php
@@ -8,3 +8,46 @@ echo "\nConsulta de egresos";
 ?>
 <h2>Consulta de egresos</h2>
 
+<body>  
+<div align='center'>  
+  <table border='1' cellpadding='0' cellspacing='0' width='600' bgcolor='#F6F6F6' bordercolor='#FFFFFF'>  
+    <tr>  
+      <td width='150' style='font-weight: bold'>ID</td>  
+      <td width='150' style='font-weight: bold'>CONCEPTO</td>  
+      <td width='150' style='font-weight: bold'>CANTIDAD</td>  
+      <td width='150' style='font-weight: bold'>FECHA</td>  
+    </tr>  
+<?php  
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+$pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$result = pg_query($pg_conn, "SELECT * FROM egreso ");     // Esta linea hace la consulta 
+
+if (!pg_num_rows($result)) {
+  print("No hay datos\n");
+} else {
+	while ($row = pg_fetch_row($result)) {
+	    echo "<tr>";
+	    echo "<td>" . $row[0] . "</td>";
+	    echo "<td>" . htmlspecialchars($row[1]) . "</td>";
+	    echo "<td>" . htmlspecialchars($row[2]) . "</td>";
+	    echo "<td>" . htmlspecialchars($row[3]) . "</td>";
+	    echo "</tr>";
+	}
+}
+pg_close($pg_conn); 
+?>  
+   </table>  
+   <form method="POST" action="ingresos.php">
+		<button type="submit">Registro de ingresos</button>
+	</form>
+	<form method="POST" action="contenido.php">
+		<button type="submit">Volver al menú</button>
+	</form>
+</div>  
+</body>  
+
+</html> 
--- egresos.php
+++ egresos.php
@@ -7,4 +7,18 @@ echo "Bienvenido " . $_SESSION['usuario'];
 
 ?>
 <h2>Registro de egresos</h2>
+<center>
+	<form method="POST" action="registrae.php">   
+	<input type="text" name="idengreso" placeholder="Clave" /> 
+    <input type="text" name="concepto" placeholder="Concepto" />
+	<br />
+	<input type="text" name="cantidad" placeholder="Cantidad" />
+
+	<br />
+	<button type="submit">Guardar egreso</button></p>     
+	</form> 
+	<form method="POST" action="contenido.php">
+		<button type="submit">Volver al menú</button>
+	</form>
+</center>
 
