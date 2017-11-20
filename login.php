a2d3f9f361b356efbbe6bbe2be2d5bfd9835c735
YairGM <yagamo@gmail.com>
2017-11-19 20:12:48 -0600

    Editing files

--- login.php
+++ login.php
@@ -1,4 +1,28 @@
-  <h2>LOGIN</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Consultorio medico</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha384-gv0oNvwnqzF6ULI9TVsSmnULNb3zasNysvWwfT/s4l8k5I+g6oFz9dye0wg3rQ2Q" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylee.css">
+
+</head>
+<body>
+
+    <div class="container">
+        <div class="page-header">
+            <h2>
+                Consultorio medico
+            </h2>
+        </div>
+        <div class="Menu">
+        <ul>
+            <li><a href="#home">Inicio</a></li>
+            <li><a href="#alta">Alta</a></li>
+            <li><a href="#consulta">Consulta</a></li>
+        </ul>
+        </div>
+        <h2>LOGIN</h2>
 
 	<center>
 		<form method="POST" action="validar.php">
@@ -9,3 +33,9 @@
 			<button type="submit">Iniciar Sesion</button>
 		</form>
 	</center>
+	 <div class="footer">
+      <p>Hecho con ❤</p>
+     </div>
+    </div>
+</body>
+</html>>
