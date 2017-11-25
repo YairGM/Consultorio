4b985aa48e3c4fd640a06018a7bd4d6afbc3f9be
YairGM <yagamo@gmail.com>
2017-11-25 17:58:21 -0600

    consulta

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
       <div class="footer">
             <p>Hecho con ❤</p>
-            <p>Codigo: <a href="https://github.com/YairGM/Consultorio">Github YairGM</a>.</p>
+            <p>Codigo: <a href="https://github.com/YairGM">Github YairGM</a>.</p>
       </div>
