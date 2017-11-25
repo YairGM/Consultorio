6593340fc353801a9ab6d613aa0d155b51db1444
YairGM <yagamo@gmail.com>
2017-11-25 15:25:02 -0600

    Editing files

--- cerrar.php
+++ cerrar.php
@@ -3,6 +3,5 @@ session_start();
 session_destroy();
 header("location: index.php");
 exit();
-
 ?>
 
--- servicio.php
+++ servicio.php
@@ -6,3 +6,19 @@ include 'footer.php';
 echo "Bienvenido " . $_SESSION['usuario'];
 ?>
 <h2>Registro de servicios</h2>
+
+<center>
+	<form method="POST" action="venta.php">   
+	<button type="submit" style="width:400px;">Registro Servicios</button>
+	<br />
+  	</form>
+  	<form method="POST" action="registras.php">   
+	<button type="submit" style="width:400px;">Registro Servicios</button>
+	<br/>
+	</form> 
+	<form method="POST" action="contenido.php">
+	<button type="submit" style="width:400px;">Volver al menú</button>
+	</form>
+</center>
+
+
